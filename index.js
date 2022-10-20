ef85a61ea91e5d489819ab799936a1068f99fe0c
ahadulislampinoy <ahadulislampinoy@gmail.com>
2022-10-21 01:02:07 +0600

    dynamic api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = 5000;
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
@@ -11,8 +12,26 @@ app.get("/", (req, res) => {
   res.send("Welcome to express");
 });
 
+//! Static API
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+//! Dyanmic API
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const categoryItems = news.filter((n) => n.category_id === id);
+  res.send(categoryItems);
 });
 
 app.listen(port, () => {
